69e88e68216005f63dfc8b9b6590dd9a50dbf797
Wisdom MBIZA <91102908+Arc2447@users.noreply.github.com>
2024-07-01 01:03:55 +0200

    Add files via upload

--- CustomerMApp/Form1.Designer.cs
+++ CustomerMApp/Form1.Designer.cs
@@ -35,15 +35,15 @@
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.welcome1 = new CustomerMApp.UserControls.Welcome();
+            this.uploadUC1 = new CustomerMApp.UserControls.UploadUC();
+            this.displayUC1 = new CustomerMApp.UserControls.DisplayUC();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Elipse3 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Elipse4 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Elipse5 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Elipse6 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.welcome1 = new CustomerMApp.UserControls.Welcome();
-            this.displayUC1 = new CustomerMApp.UserControls.DisplayUC();
-            this.uploadUC1 = new CustomerMApp.UserControls.UploadUC();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -52,11 +52,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(228, 9);
+            this.label1.Location = new System.Drawing.Point(342, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(332, 31);
+            this.label1.Size = new System.Drawing.Size(516, 47);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Costomer Managemen App";
+            this.label1.Text = "Costomer Management App";
             // 
             // panel1
             // 
@@ -64,9 +65,10 @@
             this.panel1.Controls.Add(this.guna2Button3);
             this.panel1.Controls.Add(this.guna2Button1);
             this.panel1.Controls.Add(this.guna2Button2);
-            this.panel1.Location = new System.Drawing.Point(21, 75);
+            this.panel1.Location = new System.Drawing.Point(32, 115);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 411);
+            this.panel1.Size = new System.Drawing.Size(300, 632);
             this.panel1.TabIndex = 1;
             // 
             // guna2Button3
@@ -82,9 +84,10 @@
             this.guna2Button3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.guna2Button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button3.ForeColor = System.Drawing.Color.White;
-            this.guna2Button3.Location = new System.Drawing.Point(20, 163);
+            this.guna2Button3.Location = new System.Drawing.Point(30, 251);
+            this.guna2Button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button3.Name = "guna2Button3";
-            this.guna2Button3.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button3.Size = new System.Drawing.Size(270, 69);
             this.guna2Button3.TabIndex = 4;
             this.guna2Button3.Text = "Exit App";
             this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
@@ -102,11 +105,12 @@
             this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.guna2Button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(20, 93);
+            this.guna2Button1.Location = new System.Drawing.Point(30, 143);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button1.Size = new System.Drawing.Size(270, 69);
             this.guna2Button1.TabIndex = 3;
-            this.guna2Button1.Text = "Upload Employee File";
+            this.guna2Button1.Text = "Upload  File";
             this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click_1);
             // 
             // guna2Button2
@@ -122,11 +126,12 @@
             this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.guna2Button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button2.ForeColor = System.Drawing.Color.White;
-            this.guna2Button2.Location = new System.Drawing.Point(20, 42);
+            this.guna2Button2.Location = new System.Drawing.Point(30, 65);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button2.Name = "guna2Button2";
-            this.guna2Button2.Size = new System.Drawing.Size(180, 45);
+            this.guna2Button2.Size = new System.Drawing.Size(270, 69);
             this.guna2Button2.TabIndex = 2;
-            this.guna2Button2.Text = "Display Employee";
+            this.guna2Button2.Text = "Display Database";
             this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click_1);
             // 
             // panel2
@@ -135,10 +140,38 @@
             this.panel2.Controls.Add(this.welcome1);
             this.panel2.Controls.Add(this.uploadUC1);
             this.panel2.Controls.Add(this.displayUC1);
-            this.panel2.Location = new System.Drawing.Point(210, 75);
+            this.panel2.Location = new System.Drawing.Point(315, 115);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(601, 411);
+            this.panel2.Size = new System.Drawing.Size(902, 632);
             this.panel2.TabIndex = 2;
+            // 
+            // welcome1
+            // 
+            this.welcome1.BackColor = System.Drawing.Color.White;
+            this.welcome1.Location = new System.Drawing.Point(4, 5);
+            this.welcome1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.welcome1.Name = "welcome1";
+            this.welcome1.Size = new System.Drawing.Size(902, 632);
+            this.welcome1.TabIndex = 0;
+            // 
+            // uploadUC1
+            // 
+            this.uploadUC1.BackColor = System.Drawing.Color.White;
+            this.uploadUC1.Location = new System.Drawing.Point(0, -5);
+            this.uploadUC1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.uploadUC1.Name = "uploadUC1";
+            this.uploadUC1.Size = new System.Drawing.Size(902, 632);
+            this.uploadUC1.TabIndex = 2;
+            // 
+            // displayUC1
+            // 
+            this.displayUC1.BackColor = System.Drawing.Color.White;
+            this.displayUC1.Location = new System.Drawing.Point(0, 0);
+            this.displayUC1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.displayUC1.Name = "displayUC1";
+            this.displayUC1.Size = new System.Drawing.Size(902, 632);
+            this.displayUC1.TabIndex = 1;
             // 
             // guna2Elipse1
             // 
@@ -165,40 +198,17 @@
             this.guna2Elipse6.BorderRadius = 30;
             this.guna2Elipse6.TargetControl = this.panel2;
             // 
-            // welcome1
-            // 
-            this.welcome1.BackColor = System.Drawing.Color.White;
-            this.welcome1.Location = new System.Drawing.Point(3, 3);
-            this.welcome1.Name = "welcome1";
-            this.welcome1.Size = new System.Drawing.Size(601, 411);
-            this.welcome1.TabIndex = 0;
-            // 
-            // displayUC1
-            // 
-            this.displayUC1.BackColor = System.Drawing.Color.White;
-            this.displayUC1.Location = new System.Drawing.Point(0, 0);
-            this.displayUC1.Name = "displayUC1";
-            this.displayUC1.Size = new System.Drawing.Size(601, 411);
-            this.displayUC1.TabIndex = 1;
-            // 
-            // uploadUC1
-            // 
-            this.uploadUC1.BackColor = System.Drawing.Color.White;
-            this.uploadUC1.Location = new System.Drawing.Point(0, -3);
-            this.uploadUC1.Name = "uploadUC1";
-            this.uploadUC1.Size = new System.Drawing.Size(601, 411);
-            this.uploadUC1.TabIndex = 2;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(823, 498);
+            this.ClientSize = new System.Drawing.Size(1234, 766);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
--- CustomerMApp/UserControls/UploadUC.Designer.cs
+++ CustomerMApp/UserControls/UploadUC.Designer.cs
@@ -34,6 +34,10 @@
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2Elipse1
@@ -45,9 +49,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(130, 22);
+            this.label1.Location = new System.Drawing.Point(195, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(347, 29);
+            this.label1.Size = new System.Drawing.Size(505, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Upload a CSV Customer File";
             // 
@@ -61,9 +66,10 @@
             this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.guna2Button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(228, 151);
+            this.guna2Button1.Location = new System.Drawing.Point(343, 201);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(121, 43);
+            this.guna2Button1.Size = new System.Drawing.Size(182, 66);
             this.guna2Button1.TabIndex = 1;
             this.guna2Button1.Text = "Upload File";
             this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
@@ -79,13 +85,15 @@
             this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Location = new System.Drawing.Point(117, 92);
+            this.guna2TextBox1.Location = new System.Drawing.Point(235, 142);
+            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.guna2TextBox1.Name = "guna2TextBox1";
             this.guna2TextBox1.PasswordChar = '\0';
             this.guna2TextBox1.PlaceholderText = "";
             this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.Size = new System.Drawing.Size(412, 53);
+            this.guna2TextBox1.Size = new System.Drawing.Size(572, 37);
             this.guna2TextBox1.TabIndex = 2;
+            this.guna2TextBox1.TextChanged += new System.EventHandler(this.guna2TextBox1_TextChanged);
             // 
             // guna2Button2
             // 
@@ -96,23 +104,64 @@
             this.guna2Button2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.guna2Button2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button2.ForeColor = System.Drawing.Color.White;
-            this.guna2Button2.Location = new System.Drawing.Point(70, 92);
+            this.guna2Button2.Location = new System.Drawing.Point(105, 142);
+            this.guna2Button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.guna2Button2.Name = "guna2Button2";
-            this.guna2Button2.Size = new System.Drawing.Size(118, 53);
+            this.guna2Button2.Size = new System.Drawing.Size(131, 37);
             this.guna2Button2.TabIndex = 3;
             this.guna2Button2.Text = "Choose File";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.Location = new System.Drawing.Point(105, 294);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(702, 188);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.BorderRadius = 15;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.guna2Button3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.Location = new System.Drawing.Point(357, 508);
+            this.guna2Button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(182, 66);
+            this.guna2Button3.TabIndex = 5;
+            this.guna2Button3.Text = "Save to database";
             // 
             // UploadUC
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.guna2Button2);
             this.Controls.Add(this.guna2TextBox1);
             this.Controls.Add(this.guna2Button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "UploadUC";
-            this.Size = new System.Drawing.Size(601, 411);
+            this.Size = new System.Drawing.Size(902, 632);
+            this.Load += new System.EventHandler(this.UploadUC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,5 +174,8 @@
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
